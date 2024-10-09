f4752dee348185f51ccf69a61e9ed880d71e38a8
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-09 17:41:09 +0800

    Jobs Page: Tag Component & Job Info

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\JobListingController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('', fn () => to_route('job-listing.index'));
+
 Route::resource('job-listing', JobListingController::class)->only(['index']);
