e0cd9059a6c79d656ac34977bb0fc28586c083fb
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-10 14:19:53 +0800

    Refactor Time: Displaying Input Errors

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,16 +1,36 @@
 <div class="relative">
-	@if ($formRef)
-		<button class="absolute top-0 right-0 flex h-full items-center pr-2" type="button"
-			@click="$refs['input-{{ $name }}'].value=''; $refs['{{ $formRef }}'].submit();">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-				class="size-4 text-slate-500">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-			</svg>
+	@if ('textarea' != $type)
+		@if ($formRef)
+			<button class="absolute top-0 right-0 flex h-full items-center pr-2" type="button"
+				@click="$refs['input-{{ $name }}'].value=''; $refs['{{ $formRef }}'].submit();">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+					class="size-4 text-slate-500">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+				</svg>
 
-		</button>
+			</button>
+		@endif
+
+		<input x-ref="input-{{ $name }}" type="{{ $type }}" name="{{ $name }}"
+			value="{{ old($name, $value) }}" placeholder="{{ $placeholder }}" id="{{ $name }}"
+			@class([
+				'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+				'pr-8' => $formRef,
+				'ring-slate-300' => !$errors->has($name),
+				'ring-red-300' => $errors->has($name),
+			]) />
+	@else
+		<textarea id="{{ $name }}" name="{{ $name }}" @class([
+			'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+			'pr-8' => $formRef,
+			'ring-slate-300' => !$errors->has($name),
+			'ring-red-300' => $errors->has($name),
+		])>{{ old($name, $value) }}</textarea>
 	@endif
 
-	<input x-ref="input-{{ $name }}" type="{{ $type }}" name="{{ $name }}"
-		value="{{ $value }}" placeholder="{{ $placeholder }}" id="{{ $name }}"
-		class="w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2 pr-8">
+	@error($name)
+		<div class="mt-1 text-xs text-red-500">
+			{{ $message }}
+		</div>
+	@enderror
 </div>
