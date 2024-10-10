97c12a6369a36901815499b64293450945c007a3
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-10 11:15:57 +0800

    Authentication: Sign-in Form

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -10,7 +10,7 @@
 		</button>
 	@endif
 
-	<input x-ref="input-{{ $name }}" type="text" name="{{ $name }}" value="{{ $value }}"
-		placeholder="{{ $placeholder }}" id="{{ $name }}"
+	<input x-ref="input-{{ $name }}" type="{{ $type }}" name="{{ $name }}"
+		value="{{ $value }}" placeholder="{{ $placeholder }}" id="{{ $name }}"
 		class="w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2 pr-8">
 </div>
