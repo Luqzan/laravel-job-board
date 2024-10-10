34d194fb6cc9b13d2667af23e32ddf678221faec
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-10 10:59:22 +0800

    Authentication: The Controller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobListingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', fn () => to_route('job-listing.index'));
-
 Route::resource('job-listing', JobListingController::class)->only(['index', 'show']);
+
+Route::get('login', fn () => to_route('auth.create'))->name('login');
+Route::resource('auth', AuthController::class)->only(['create', 'store']);
