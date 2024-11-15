0c30d60435cfa85f45bdc77ec55ca23520186d2f
Luqzan Ariff <luqzanariff@gmail.com>
2024-11-15 13:54:54 +0800

    converted tailwind config to use ESM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import forms from "@tailwindcss/forms";
+
 export default {
     content: [
         "./resources/**/*.blade.php",
@@ -8,5 +10,5 @@ export default {
     theme: {
         extend: {},
     },
-    plugins: [require("@tailwindcss/forms")],
+    plugins: [forms],
 };
